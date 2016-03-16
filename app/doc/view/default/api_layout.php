f0482ab99527f4b596448c039f0f931544d10fe4
peter.wang <hpu423@126.com>
2016-03-16 10:45:46 +0800

    名称修改
    名称修改

--- app/doc/view/default/api_layout.php
+++ app/doc/view/default/api_layout.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $this->section('title')?>-牛牛API文档系统</title>
+    <title><?php echo $this->section('title')?>-牛牛文档系统</title>
     <!-- zui -->
     <link href="<?php $this->assets('doc');?>/css/zui.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php $this->assets('doc');?>/css/zui-theme.css">
